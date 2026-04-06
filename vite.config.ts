69c15b7618845458bc8e118139941489afa89926
Estifanos Kelemu <estifk6@gmail.com>
2026-04-06 12:50:50 +0300

    chore: increase Workbox cache limit and include generated service worker assets

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 3000000, // Increase limit to 3MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
